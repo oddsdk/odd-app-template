d280e39a9e2da7a8829a117fd7103f528b577791
Brian Ginsburg <gins@brianginsburg.com>
2022-08-01 15:11:32 -0700

    Add initialization error toast

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,34 @@
 <script lang="ts">
-  import { get } from 'svelte/store'
-
   import '../global.css'
   import { initialize } from '$lib/common/webnative'
   import { sessionStore, theme } from '../stores'
   import { storeTheme } from '$lib/theme'
+  import type { Session } from '$lib/session'
+  import Toast from '$components/notifications/Toast.svelte'
   import Header from '$components/Header.svelte'
+
+  let session: Session = null
 
   theme.subscribe(val => {
     storeTheme(val)
+  })
+
+  sessionStore.subscribe(val => {
+    session = val
   })
 
   const init = async () => {
     await initialize()
 
     // TODO: Remove this debugging statement
-    const session = get(sessionStore)
     console.log('session at init', session)
+  }
+
+  const clearNotification = () => {
+    sessionStore.update(session => ({
+      ...session,
+      error: null
+    }))
   }
 
   init()
@@ -24,6 +36,10 @@
 
 <div data-theme={$theme}>
   <Header />
+
+  {#if session.error}
+    <Toast kind="error" message={session.error} on:clear={clearNotification} />
+  {/if}
 
   <slot />
 </div>
