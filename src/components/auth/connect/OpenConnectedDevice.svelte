314f305d741be7941f8848523a17e6ac800a5dc7
Brian Ginsburg <gins@brianginsburg.com>
2022-08-26 12:35:40 -0700

    Add connect existing account modal

---
+++ src/components/auth/connect/OpenConnectedDevice.svelte
@@ -0,0 +1,24 @@
+<input
+  type="checkbox"
+  id="open-connected-device-modal"
+  checked
+  class="modal-toggle"
+/>
+<div class="modal">
+  <div class="modal-box w-96 relative text-center">
+    <a href="/" class="btn btn-xs btn-circle absolute right-2 top-2">✕</a>
+    <div>
+      <h3 class="mb-5 text-2xl font-serif">Connect your existing account</h3>
+      <div>
+        <p class="text-sm mb-3">
+          To connect your existing account on this device, you'll need a device
+          you are already connected on.
+        </p>
+        <p class="text-sm">
+          On that device, click "Connect a new device" and follow the
+          instuctions.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
