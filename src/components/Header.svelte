3daa3e43708b1257ddfe748b35db2be2ffd7d3b2
Brian Ginsburg <gins@brianginsburg.com>
2022-08-28 17:31:49 -0700

    Replace hamburger menu with brand
    We aren't using the menu yet, so best not to have it.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,6 +3,8 @@
   import { sessionStore, theme } from '../stores'
   import { storeTheme, type Theme } from '$lib/theme/index'
 
+  import { appName } from '$lib/app-name'
+  import Brand from '$components/icons/Brand.svelte'
   import Shield from '$components/icons/Shield.svelte'
   import LightMode from '$components/icons/LightMode.svelte'
   import DarkMode from '$components/icons/DarkMode.svelte'
@@ -14,22 +16,9 @@
 </script>
 
 <header class="navbar bg-base-100 pt-0">
-  <div class="flex-1">
-    <button class="btn btn-sm btn-square btn-ghost">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="inline-block w-7 h-7 stroke-current"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M4 6h16M4 12h16M4 18h16"
-        />
-      </svg>
-    </button>
+  <div class="flex-1" on:click={() => goto('/')}>
+    <Brand />
+    <span class="text-xl ml-2">{appName}</span>
   </div>
 
   <span class="mr-2">
