7b2bb558ed7887058740803ba1a60b7d1c42d38d
Jess Martin <jessmartin@gmail.com>
2022-08-30 13:44:16 -0400

    Update the copy

--- src/components/auth/connect/OpenConnectedDevice.svelte
+++ src/components/auth/connect/OpenConnectedDevice.svelte
@@ -8,11 +8,11 @@
   <div class="modal-box w-96 relative text-center">
     <a href="/" class="btn btn-xs btn-circle absolute right-2 top-2">✕</a>
     <div>
-      <h3 class="mb-5 text-2xl font-serif">Connect your existing account</h3>
+      <h3 class="mb-5 text-2xl font-serif">Connect an existing account</h3>
       <div>
         <p class="text-sm mb-3">
-          To connect your existing account on this device, you'll need a device
-          you are already connected on.
+          To connect with an existing account, you'll need a device that's
+          already connected to that account.
         </p>
         <p class="text-sm">
           On that device, click "Connect a new device" and follow the
