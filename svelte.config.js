2a9d4a353819e4e3653c556c1d3334ab7dd5c6ee
Jess Martin <jessmartin@gmail.com>
2022-08-30 18:51:18 -0400

    Adding netlify stuffs

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
