423ebe55b4d1b58e81198d2a98bb1d8453579715
Jess Martin <jessmartin@gmail.com>
2022-08-31 09:54:59 -0400

    Remove netlify-specific info

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,13 @@
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: preprocess({
     postcss: true,
   }),
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
+    adapter: adapter(),
   }
 }
 
