43ccebd61d06f82b556d44c3b5ea39bdaf901317
Brian Ginsburg <gins@brianginsburg.com>
2022-10-14 14:40:48 -0700

    Add info and warning notification icons

---
+++ src/components/icons/WarningThinIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let color: string
+</script>
+
+<svg
+  width="36"
+  height="32"
+  viewBox="0 0 36 32"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M17.9998 12.5V16.1667M17.9998 23.5H18.0182M5.29811 30.8333H30.7015C33.5241 30.8333 35.2883 27.7778 33.877 25.3333L21.1752 3.33333C19.7639 0.888889 16.2357 0.888889 14.8244 3.33333L2.12269 25.3333C0.711386 27.7778 2.47551 30.8333 5.29811 30.8333Z"
+    stroke={color}
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
--- src/components/notifications/Notification.svelte
+++ src/components/notifications/Notification.svelte
@@ -4,6 +4,8 @@
   import { themeStore } from '../../stores'
   import type { Notification } from '$lib/notifications'
   import CheckThinIcon from '$components/icons/CheckThinIcon.svelte'
+  import InfoThinIcon from '$components/icons/InfoThinIcon.svelte'
+  import WarningThinIcon from '$components/icons/WarningThinIcon.svelte'
   import XThinIcon from '$components/icons/XThinIcon.svelte'
 
   export let notification: Notification
@@ -24,6 +26,10 @@
         />
       {:else if notification.type === 'error'}
         <XThinIcon color={$themeStore === 'light' ? '#ffd6d7' : '#fec3c3'} />
+      {:else if notification.type === 'info'}
+        <InfoThinIcon color="#1e3a8a" />
+      {:else if notification.type === 'warning'}
+        <WarningThinIcon color="#7c2d12" />
       {/if}
       <span class="pl-1">{@html notification.msg}</span>
     </div>
