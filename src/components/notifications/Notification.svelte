3b5cabee1124979df908ef507c670c1a900b6820
Brian Ginsburg <7957636+bgins@users.noreply.github.com>
2022-10-14 19:26:30 -0700

    Add info and warning notification icons (#76)
    * Add info and warning notification icons
    
    * Switch over to icon map

--- src/components/notifications/Notification.svelte
+++ src/components/notifications/Notification.svelte
@@ -4,9 +4,38 @@
   import { themeStore } from '../../stores'
   import type { Notification } from '$lib/notifications'
   import CheckThinIcon from '$components/icons/CheckThinIcon.svelte'
+  import InfoThinIcon from '$components/icons/InfoThinIcon.svelte'
+  import WarningThinIcon from '$components/icons/WarningThinIcon.svelte'
   import XThinIcon from '$components/icons/XThinIcon.svelte'
 
   export let notification: Notification
+
+  const iconMap = {
+    info: {
+      component: InfoThinIcon,
+      props: {
+        color: '#1e3a8a'
+      }
+    },
+    error: {
+      component: XThinIcon,
+      props: {
+        color: $themeStore === 'light' ? '#ffd6d7' : '#fec3c3'
+      }
+    },
+    success: {
+      component: CheckThinIcon,
+      props: {
+        color: $themeStore === 'light' ? '#b8ffd3' : '#002e12'
+      }
+    },
+    warning: {
+      component: WarningThinIcon,
+      props: {
+        color: '#7c2d12'
+      }
+    }
+  }
 </script>
 
 <div
@@ -18,13 +47,10 @@
 >
   <div class="alert alert-{notification.type} text-sm mb-3 peer-last:mb-0">
     <div>
-      {#if notification.type === 'success'}
-        <CheckThinIcon
-          color={$themeStore === 'light' ? '#b8ffd3' : '#002e12'}
-        />
-      {:else if notification.type === 'error'}
-        <XThinIcon color={$themeStore === 'light' ? '#ffd6d7' : '#fec3c3'} />
-      {/if}
+      <svelte:component
+        this={iconMap[notification.type].component}
+        {...iconMap[notification.type].props}
+      />
       <span class="pl-1">{@html notification.msg}</span>
     </div>
   </div>
