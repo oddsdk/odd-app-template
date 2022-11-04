70f294b9b763890703bd8f611cf28591d1f348ab
avivash <av@andrewvivash.com>
2022-11-04 09:45:35 -0700

    Chore: update public homepage content and add connect btn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
   import { sessionStore } from '../stores'
-  import { appName } from '$lib/app-info'
-  import AboutThisTemplate from '$components/home/AboutThisTemplate.svelte'
-  import Home from '$components/home/Home.svelte'
+  import Authed from '$components/home/Authed.svelte'
+  import Public from '$components/home/Public.svelte'
 </script>
 
 {#if $sessionStore?.authed}
-  <Home />
+  <Authed />
 {:else}
-  <div
-    class="min-h-[calc(100vh-96px)] flex flex-col items-start justify-center max-w-[700px] m-auto gap-6 pb-5 text-sm"
-  >
-    <AboutThisTemplate title={`Welcome to the ${appName}`} />
-  </div>
+  <Public />
 {/if}
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-  import AboutThisTemplate from '$components/home/AboutThisTemplate.svelte'
+  import AboutThisTemplate from '$components/about/AboutThisTemplate.svelte'
 </script>
 
-<div
-  class="min-h-[calc(100vh-128px)] md:min-h-[calc(100vh-160px)] pt-8 md:pt-16 flex flex-col items-start max-w-[690px] m-auto gap-10 pb-5 text-sm"
->
-  <AboutThisTemplate />
-</div>
+<AboutThisTemplate />
