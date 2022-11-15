80a3b051be3f4c70c94c6fc9f97141d6b90e4780
Steven Vandevelde <icid.asset@gmail.com>
2022-11-15 12:29:48 +0100

    Linking

--- src/lib/auth/linking.ts
+++ src/lib/auth/linking.ts
@@ -1,14 +1,28 @@
 import * as webnative from 'webnative'
-import type { account } from 'webnative'
+import { get as getStore } from 'svelte/store'
+
+import { programStore } from '$src/stores'
+
 
 export const createAccountLinkingConsumer = async (
   username: string
-): Promise<account.AccountLinkingConsumer> => {
-  return await webnative.account.createConsumer({ username })
+): Promise<webnative.AccountLinkingConsumer> => {
+  const program = getStore(programStore)
+  if (program) return program.auth[ webnative.strategyTypes.default ].accountConsumer(username)
+
+  // Wait for program to be initialised
+  return new Promise((resolve) => {
+    programStore.subscribe(program => {
+      if (!program) return
+      const consumer = program.auth[ webnative.strategyTypes.default ].accountConsumer(username)
+      resolve(consumer)
+    })
+  })
 }
 
 export const createAccountLinkingProducer = async (
   username: string
-): Promise<account.AccountLinkingProducer> => {
-  return await webnative.account.createProducer({ username })
+): Promise<webnative.AccountLinkingProducer> => {
+  const program = getStore(programStore)
+  return program.auth[ webnative.strategyTypes.default ].accountProducer(username)
 }
