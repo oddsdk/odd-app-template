4b015afa9dc1364f09a990d1c286f88f83786d83
avivash <av@andrewvivash.com>
2022-12-14 15:49:12 -0600

    Fix: revert alpha tag changes

--- src/components/nav/AlphaTag.svelte
+++ src/components/nav/AlphaTag.svelte
@@ -1,5 +1,5 @@
 <span
-  class="inline-block px-2 py-[2px] !no-underline font-medium text-center text-xs text-neutral-900 bg-gradient-to-r from-orange-600 to-orange-300"
+  class="inline-block px-2 py-[2px] !no-underline font-medium text-center text-xs text-neutral-900 bg-gradient-to-r from-orange-300 to-orange-600"
 >
   ALPHA
 </span>
