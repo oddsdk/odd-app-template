e2073c506f26b85b4051ef1005f657df49c652b6
avivash <av@andrewvivash.com>
2023-01-06 15:38:40 -0800

    Fix: update test path and add more browsers

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5173');
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Webnative SDK Demo/)
 });
 
 test('get register link', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5173');
+  await page.goto('/');
 
   // Click the register link.
   await page.getByRole('link', { name: 'Connect this device' }).click();
