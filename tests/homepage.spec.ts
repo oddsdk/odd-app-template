08bcf9af92a96862b57d0aa285a24c0e33335731
avivash <av@andrewvivash.com>
2023-01-06 16:56:47 -0800

    Chore: remove unnecssary test(the register link accomplishes the goal of the first test)

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,18 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('/');
+import { appName } from '../src/lib/app-info'
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Webnative SDK Demo/)
-});
-
-test('get register link', async ({ page }) => {
-  await page.goto('/');
+test('find and click register link', async ({ page }) => {
+  await page.goto('/')
 
   // Click the register link.
-  await page.getByRole('link', { name: 'Connect this device' }).click();
+  await page.getByRole('link', { name: 'Connect this device' }).click()
 
   // Expects the URL to contain register.
-  await expect(page).toHaveURL(/.*register/);
-});
+  await expect(page).toHaveURL(/.*register/)
+})
