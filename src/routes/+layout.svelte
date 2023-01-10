dd2454c61fd63c3ea4da6a1bb8ee6fdbc87f5661
avivash <av@andrewvivash.com>
2023-01-09 16:00:52 -0800

    Fix: move meta charset to app.html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,7 +27,6 @@
 
 <svelte:head>
   <title>{appName}</title>
-  <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="robots" content="index,follow" />
   <meta name="googlebot" content="index,follow" />
