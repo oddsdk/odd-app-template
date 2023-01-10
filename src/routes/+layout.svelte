9f4b7e2b5d3f318d4d6742f7d2ec8160f2b4b7e4
avivash <av@andrewvivash.com>
2023-01-09 16:28:49 -0800

    Fix: move static meta tags to app.html

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,29 +27,16 @@
 
 <svelte:head>
   <title>{appName}</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="robots" content="index,follow" />
-  <meta name="googlebot" content="index,follow" />
+
   <meta name="description" content={appDescription} />
   <meta property="og:title" content={appName} />
   <meta property="og:description" content={appDescription} />
   <meta property="og:url" content={appURL} />
-  <meta property="og:type" content="website" />
   <meta property="og:image" content={appImageURL} />
-  <meta property="og:image:alt" content="WebNative Template" />
-  <meta property="og:image:width" content="1250" />
-  <meta property="og:image:height" content="358" />
-  <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content={appName} />
   <meta name="twitter:description" content={appDescription} />
   <meta name="twitter:image" content={appImageURL} />
   <meta name="twitter:image:alt" content={appName} />
-
-  <!-- See https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs for description. -->
-  <link rel="icon" href="/favicon.ico" sizes="any" />
-  <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <link rel="manifest" href="/manifest.webmanifest" />
 </svelte:head>
 
 <div data-theme={$themeStore.selectedTheme} class="min-h-screen">
