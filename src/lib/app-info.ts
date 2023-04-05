d84a2ca6b691f693b9129042e3ac4ffe5db83e55
avivash <av@andrewvivash.com>
2023-04-05 15:49:13 -0700

    Fix: WAT -> OAT

--- src/lib/app-info.ts
+++ src/lib/app-info.ts
@@ -3,4 +3,4 @@ export const appDescription = 'This is another awesome ODD app.'
 export const appURL = 'https://odd.netlify.app'
 export const appImageURL = `${appURL}/preview.png`
 export const ipfsGatewayUrl = 'runfission.com'
-export const oddNamespace = { creator: 'Fission', name: 'WAT' }
+export const oddNamespace = { creator: 'Fission', name: 'OAT' }
